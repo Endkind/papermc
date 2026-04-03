0df9d8ba603b0f0473e086462591f4e5f951358f
Endkind <endkind.ender@gmail.com>
2026-04-03 22:34:26 +0200

    feat: add a report page for version check results

--- .github/scripts/utils/version.py
+++ .github/scripts/utils/version.py
@@ -12,3 +12,21 @@ class VersionUtils:
         ]
 
         return local_versions
+
+    @classmethod
+    def get_all_local_disabled_versions(cls) -> list[str]:
+        current_path = Path(__file__).parent
+        local_version_path = current_path.parent.parent.parent / "versions"
+
+        disabled_versions = []
+
+        for entry in local_version_path.iterdir():
+            if entry.is_dir():
+                disabled = False
+                for version_entry in entry.iterdir():
+                    if version_entry.is_file() and version_entry.name == ".disabled":
+                        disabled = True
+                        break
+                if disabled:
+                    disabled_versions.append(entry.name)
+        return disabled_versions
