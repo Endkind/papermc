ca8afa1c2ea346505050fdca126cb746dc043869
Endkind <endkind.ender@gmail.com>
2026-07-10 17:13:20 +0200

    fix: handle PaperMC API errors and improve version fetching logic

--- .github/scripts/utils/papermc_api.py
+++ .github/scripts/utils/papermc_api.py
@@ -1,21 +1,27 @@
 import requests
 from config import PaperMCAPIConfig
 from enums import PaperMCAPIProject
+from result import Err, Ok, Result
 from yarl import URL
 
 
 class PaperMCAPIUtils:
     @classmethod
-    def get_all_versions(cls, project: PaperMCAPIProject) -> list[str]:
+    def get_all_versions(cls, project: PaperMCAPIProject) -> Result[list[str], str]:
         base_url = URL(PaperMCAPIConfig.BASE_URL)
 
         url = base_url / project.value
 
         response = requests.get(url.__str__())
 
+        if response.status_code != 200:
+            return Err(
+                f"Failed to fetch versions for project {project.value}. Status code: {response.status_code}"
+            )
+
         all_versions = []
 
         for _, versions in response.json()["versions"].items():
             all_versions.extend(versions)
 
-        return all_versions
+        return Ok(all_versions)
